032cc724e92621752d4c271804a87c4eb3d10cde
Dawid Stasiak <dawid.stasiak21@gmail.com>
2022-06-27 18:53:38 +0200

    create simplest without texture

--- f_simplest.glsl
+++ f_simplest.glsl
@@ -1,23 +1,30 @@
 #version 330
 
-in vec4 i_color;
-in vec4 l;
-in vec4 n;
-in vec4 v;
-
 out vec4 pixelColor; //Output variable. Almost final pixel color.
 
-void main(void) {
+//Varying variables
+in vec4 ic;
+in vec4 n;
+in vec4 l;
+in vec4 v;
+in vec4 i_kd;
+in vec4 i_ka;
+in vec4 i_ks;
+in float i_alpha;
 
+void main(void) {
 	vec4 ml = normalize(l);
 	vec4 mn = normalize(n);
 	vec4 mv = normalize(v);
-
 	vec4 mr = reflect(-ml, mn);
-	float nl = clamp(dot(mn, ml), 0, 1);
-    float rv = clamp(dot(mr, mv), 0, 1);
-    rv = pow(rv, 25); // change 25 until it looks good
 
-	//pixelColor = i_color * nl + rv;
-	pixelColor = i_color;
+	//Surface parameters
+	vec4 kd = i_kd;
+	vec4 ks = i_ks;
+	vec4 ka = i_ka;
+
+	//Lighting model computation
+	float nl = clamp(dot(mn, ml), 0, 1);
+	float rv = pow(clamp(dot(mr, mv), 0, 1), i_alpha);
+	pixelColor = vec4(ka.rgb * vec3(0.1f), 0) + vec4(kd.rgb * nl, kd.a) + vec4(ks.rgb * rv, 0);
 }
